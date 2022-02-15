043da46098556a794726e66d941376902e53891c
Chathuranga97lj <chathuit97@gmail.com>
2022-02-15 18:10:48 +0530

    Create Hello world program

---
+++ src/com/itum/Main.java
@@ -0,0 +1,11 @@
+package com.itum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Out folder is a contains with byte code files
+    }
+}
+
+// git init - initialize git repo
+// git add. - add all the files
